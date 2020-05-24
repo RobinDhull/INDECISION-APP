3f3349e1dde5a63df649fea895e8b58bc46730bf
Robin Dhull <rsdhull707@gmail.com>
2020-05-24 09:38:54 +0530

    Added conditional statements

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,76 @@
 'use strict';
 
 // JSX - JavaScript XML
+var app = {
+  title: 'Indecision App',
+  subtitle: 'This is some info'
+};
+
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  ' Does this change'
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
 );
+
+var user = {
+  name: 'Robin',
+  age: 20,
+  location: 'Chandigarh'
+};
+
+function getLocation(location) {
+  if (location) {
+    return location;
+  } else {
+    return 'Unknown';
+  }
+}
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name.toUpperCase()
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: ',
+    getLocation(user.location)
+  )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,5 +1,42 @@
 // JSX - JavaScript XML
-var template = <h1> Does this change</h1>;
+var app = {
+  title: 'Indecision App',
+  subtitle: 'This is some info'
+}
+
+var template = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+var user = {
+  name: 'Robin',
+  age: 20,
+  location: 'Chandigarh'
+}
+
+function getLocation(location) {
+  if (location) {
+    return location;
+  } else {
+    return 'Unknown';
+  }
+}
+
+var templateTwo = (
+  <div>
+    <h1>{user.name.toUpperCase()}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {getLocation(user.location)}</p>
+  </div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
